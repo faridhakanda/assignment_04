59b29a1f60fdebf20a32841e6fb454f2026b4728
faridhakanda <faridakanda1729@gmail.com>
2026-02-22 19:39:25 +0600

    header and job counter section is done

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 console.log("Assignment-04 project is setup successfully!");
+
+
+
+function showOnly(id) {
+    const interview = document.getElementById("interview");
+    const rejected = document.getElementById("rejected");
+    const available = document.getElementById("jobs");
+    
+    available.classList.add("hidden");
+    interview.classList.add("hidden");
+    rejected.classList.add("hidden");
+
+    
+    const selectedId = document.getElementById(id);
+    selectedId.classList.remove("hidden");
+    //console.log("clicked")
+}
+
+
